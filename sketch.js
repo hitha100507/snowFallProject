8077b91bfee53e5eb3f26f85f1ec9eae7f928d80
hitha100507 <75884085+hitha100507@users.noreply.github.com>
2021-08-28 14:05:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine=Matter.Engine; 
+const World=Matter.World; 
+const Bodies=Matter.Bodies; 
+var engine,world; 
+var bgimg; var snow=[];
+ var snow1img,snow2img;
+  function preload(){ 
+    bgimg=loadImage("snow3.jpg");
+  }
+function setup() {
+  engine=Engine.create(); 
+  world=engine.world;
+  createCanvas(800,400);
+  if(frameCount % 50 === 0){ 
+    for(var i=0; i<200; i++)
+    { snow.push(new Snow(random(0,800), random(0,800))); } 
+  }
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  background(bgimg); 
+  Engine.update(engine); 
+  for(var i = 0; i<200; i++)
+  { snow[i].showDrop(); snow[i].updateY()
+   }
+  drawSprites();
+}
